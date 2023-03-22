a9b9830498d7989b178653d8a5836c670d91d93b
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2023-03-22 09:23:57 -0300

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:listadetarefas/screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Lista de tarefas',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home: Container(),
+      home: HomeScreen(),
     );
   }
 }
